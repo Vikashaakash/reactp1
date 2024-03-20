0b14b19263c1492129d894ce5c21bfed868cb77e
VIKASH KUMAR <94210571+Vikashaakash@users.noreply.github.com>
2024-03-20 16:43:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import "./style/App.scss";
+import "./style/Header.scss";
+import "./style/Home.scss";
+import "./style/Footer.scss";
+import "./style/Contact.scss";
+import "./style/mediaquery.scss";
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
